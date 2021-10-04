c3c8b93e6ae7ec567be6e18130689a7e233ab819
dfmartin43 <58089053+dfmartin43@users.noreply.github.com>
2021-10-04 18:15:10 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
-from distutils.core import setup
+#from distutils.core import setup
+#setup(name='test',
+#      version='1.0',
+#      #py_modules=['testUIOP'],
+#      packages=['exampleproject'],
+#      )
+
+from setuptools import setup
+
 setup(name='test',
-      version='1.0',
-      #py_modules=['testUIOP'],
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/dfmartin43/test',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
       packages=['exampleproject'],
-      )
+      zip_safe=False)
