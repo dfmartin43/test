ee2b0c2ea22f43f871f50c2e61ba9d603075f9f3
dfmartin43 <58089053+dfmartin43@users.noreply.github.com>
2021-10-04 16:38:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
-from distutils.core import setup
+#from distutils.core import setup
 
+#setup(name='test',
+#      packages = ['test'],
+#      version='0.1',
+#      description='A Test Document',
+#      url='https://github.com/dfmartin43/test',
+#      download_url = 'https://github.com/dfmartin43/test.git',
+#      install_requires=['pandas','numpy',]
+#)
+
+from distutils.core import setup
 setup(name='test',
-      packages = ['test'],
-      version='0.1',
-      description='A Test Document',
-      url='https://github.com/dfmartin43/test',
-      download_url = 'https://github.com/dfmartin43/test.git',
-      install_requires=['pandas','numpy',]
-)
+      version='1.0',
+      py_modules=['test'],
+      )
