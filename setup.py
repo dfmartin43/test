9a02ebeeac740f2e3285fb8f1f4a70936a776324
dfmartin43 <58089053+dfmartin43@users.noreply.github.com>
2021-10-04 15:45:10 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,12 @@
 from distutils.core import setup
 
 setup(name='Devon_test',
-      packages = ['wearablecompute'],
+      packages = ['test'],
       version='0.1',
-      description='Feature Engineering for Longitudinal Wearable Sensors',
-      url='https://github.com/brinnaebent/wearablecompute',
+      description='A Test Document',
+      url='https://github.com/dfmartin43/test',
       download_url = 'https://github.com/brinnaebent/wearablevar/archive/0.4.tar.gz',
-      author='Brinnae Bent',
-      author_email='runsdata@gmail.com',
-      keywords = ['wearables', 'mhealth', 'features', 'feature engineering'],
-      license='MIT',
-
-      install_requires=['pandas','numpy','datetime', 'scipy',],
-
-      classifiers=[
-        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Intended Audience :: Developers',      
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',   
-        'Programming Language :: Python :: 3',      
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        ],
+      author='Devon Martin',
+      author_email='dmarti22@ncsu.edu',
+      install_requires=['pandas','numpy',],
 )
