060f8bc09c2d330efeff92934a718fc63eb54bd9
dfmartin43 <58089053+dfmartin43@users.noreply.github.com>
2021-10-05 11:57:43 -0400

    Update __init__.py

--- exampleproject/__init__.py
+++ exampleproject/__init__.py
@@ -1,3 +1,3 @@
 def print_a_joke():
-  print("HELP ME")
+  print("AN introverted mathematician is the one staring at their own feet. The extroverted mathematician is the one who is staring at other people's feet.")
 #  return(y)
